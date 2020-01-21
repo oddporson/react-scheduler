ca713740fae1dfab47ab8c5c295a5337bd00a12a
Porson <porsonlee@gmail.com>
2020-01-21 14:37:53 -0800

    fixed selected component which show be showing the white background by adding a classnames inside the interviewerlistitem function

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "components/InterviewerListItem.scss"
+import classnames from "classnames";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classnames ("interviewers_item", {
+    "interviewers__item--selected": props.selected
+  });
   return (
-    <li className="interviewers__item">
-      <img 
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+    <li className={interviewerClass}>
+      <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
       />
       Sylvia Palmer
     </li>
-  )
+  );
 }
