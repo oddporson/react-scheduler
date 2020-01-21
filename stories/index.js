2484af165fa6d6722dfedf0c64b6484449f828eb
Porson <porsonlee@gmail.com>
2020-01-20 16:20:05 -0800

    added button stories and implemented functionality and applied css to it

--- stories/index.js
+++ stories/index.js
@@ -6,14 +6,15 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem"; // import from DayListItem.js
 
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
-  .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Base", () => <Button onClick={action("button-clicked")}>Base</Button>)
+  .add("Confirm", () => <Button confirm onClick={action("button-clicked")}>Confirm</Button>)
+  .add("Danger", () => <Button danger onClick={action("button-clicked")}>Cancel</Button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
@@ -21,4 +22,16 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+
+storiesOf("DayListItem", module) //Initiates storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
