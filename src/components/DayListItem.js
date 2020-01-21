410f408a4ba591ae1a38c2a5e973c1ecde970666
Porson <porsonlee@gmail.com>
2020-01-20 18:22:56 -0800

    fixed hover over on not available component that is supposed to lower the opacity

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,13 @@
 import React from "react";
-import classnames from "classnames";
 import "components/DayListItem.scss";
+import classnames from "classnames";
 
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spot === 0
-  })
-
+   "day-list__item--selected": props.selected,
+   "day-list__item--full": props.spots === 0
+  });
+ 
   const formatSpots = function(spots) {
     if (spots === 0) {
       return "no spots remaining"
@@ -17,8 +17,7 @@ export default function DayListItem(props) {
       return spots + " spots remaining"
   }
 }
-
-  return (
+  return(
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
