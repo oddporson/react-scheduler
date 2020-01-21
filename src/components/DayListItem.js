efffe7921787740cb26c599b518778b5b25270d3
Porson <porsonlee@gmail.com>
2020-01-20 17:43:22 -0800

    test resulted few fails. implemented a conditional function component named formatSpots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,20 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spot === 0
   })
 
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return "no spots remaining"
+    } else if (spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return spots + " spots remaining"
+  }
+}
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
