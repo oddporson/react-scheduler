18c0c1779045eec048e72a261a8987b2f492c210
Porson <porsonlee@gmail.com>
2020-01-22 13:08:51 -0800

    coded conditional to render show or empty based on props.interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import "components/Appointment/styles.scss";
+
+// Displays Appointment
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment">{props.time}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+    </article>
   )
 }
