a6a5ea6774cf800adb59949e9d54af9dafcfb150
Porson <porsonlee@gmail.com>
2020-01-21 17:01:52 -0800

    fixed issue preslected component to display the props.value

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,6 +5,7 @@ import "components/InterviewerList.scss"
 
 
 export default function InterviewerList(props) {
+  
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
@@ -12,7 +13,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={event => props.setInterviewer(interviewer.id)}
+        setInterviewer={event => props.onChange(interviewer.id)}
       />
     );
   });
