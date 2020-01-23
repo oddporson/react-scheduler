130e7ad50964e9802bbdb526abe1f49cf238ea71
Porson <porsonlee@gmail.com>
2020-01-22 21:03:43 -0800

    removed hardcoded days array and need to resolve the error on console log error stating that it encountered two children with the same key 2

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,27 +4,6 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-
 const appointments = [
   {
     id: 1,
