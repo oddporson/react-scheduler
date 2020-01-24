47bac03e565c5733c78eec4d03d7083095b8abab
Porson <porsonlee@gmail.com>
2020-01-24 14:34:08 -0800

    fetched interview ata, updated interview object in state, created selector test function

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -7,7 +7,7 @@ export default function Show(props) {
         <h2 className="textz--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3> 
+          <h3 className="text--regular">{props.interviewer.name}</h3> 
           {/* replace Sylvia Palmer with {props.interviewer.name} after you fetch interview api */}
         </section>
       </section>
