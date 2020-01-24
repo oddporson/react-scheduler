755725da41a6dc94f289af17ccf4cc3d5fe76cab
Porson <porsonlee@gmail.com>
2020-01-23 19:38:26 -0800

    all test passed but need to attempt another way to write the test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,21 @@
 const getAppointmentsForDay = function(state, day) {
-  const findDay = state.days.find(weekday => (weekday.name === day ));
-    if(!findDay) {
+
+  const findDay = state.days.find(weekDay => (weekDay.name === day ));
+  console.log("state days--->:", state.days);
+  console.log("day --->", day);
+  if(!findDay) {
       return [];
     }
-  const arrayReturned = findDay.appointments.map(id => state.appointments[id])
-    return arrayReturned;
+    
+    const arrayReturned = findDay.appointments.map(id => state.appointments[id])
+    return arrayReturned; 
+  
 }
 
 module.exports = { getAppointmentsForDay }
+
+  // console.log("state days--->:", state.days.find)
+  // console.log("day -->", day)
+  // const foo = state.days;
+  // console.log("woah -->", foo)
+  // console.log("name -->", foo.name)
