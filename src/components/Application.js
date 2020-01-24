c04678e5286a6df41546ff6842455b348243ced9
Porson <porsonlee@gmail.com>
2020-01-23 20:28:30 -0800

    used promise to fetch appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -67,8 +67,11 @@ export default function Application(props) {
     Promise.all([
       Promise.resolve(days),
       Promise.resolve(appointments)
-    ]).then((all) => 
+    ]).then((all) => {
       console.log(all)
+      // useState({day: "Monday", days: [], appointments: {}})
+      setState( prev => ({ ...prev, days:all[0].data, appointments:all[1].data }));
+    }
     )
   }, [])
 
