d140412e8ccd7fdb06308460646b4857b3214d3c
Porson <porsonlee@gmail.com>
2020-01-23 21:02:26 -0800

    remember to go back here to update interviewer name after fetching in interview API data

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -7,7 +7,8 @@ export default function Show(props) {
         <h2 className="textz--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">Sylvia Palmer</h3> 
+          {/* replace Sylvia Palmer with {props.interviewer.name} after you fetch interview api */}
         </section>
       </section>
       <section className="appointment__card-right">
