7281827254537aa3a95dc29416b378630d9c4311
Porson <porsonlee@gmail.com>
2020-01-26 02:43:02 -0800

    nothing was edited

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors"
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
  
