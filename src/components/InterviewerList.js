3e75707fe69a1209d1e213126c0295267acf4df4
Porson <porsonlee@gmail.com>
2020-01-26 17:08:59 -0800

    Revert "left a comment"
    This reverts commit c03768f23ad9f8aaeae11cf7b8dda9785054e843.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,7 +7,7 @@ import "components/InterviewerList.scss"
 export default function InterviewerList(props) {
 
   const interviewersListItem = Object.values(props.interviewers).map(interviewer => {
-    // props.interviewers was showing undefined. Use object.values to show all objects from fetched data of api/interviewers
+    // props.interviewers was showing undefined because it cannot 
     return (
       <InterviewerListItem
         key={interviewer.id}
