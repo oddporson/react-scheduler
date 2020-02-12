4ae5bc6ca6a8721180aabc57cb1adb700f38ac4e
Porson <porsonlee@gmail.com>
2020-02-12 12:25:56 -0800

    fixed issue where app crashes when user accidentally save appointment without a name and selected interviewer.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,8 @@ export default function Form(props) {
   // useState is a custom hook store it as a memory so when the function is run, it doesn't go away. it remembers it and re use it when you refresh a browser page.
   const [name, setName] = useState(props.name || "")
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+  
   //Resetting Inputs
   const reset = () => {
     setName("");
@@ -17,6 +19,21 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  // empty student name and unselected interviewer should not save
+  function saveError() {
+    if (name === "") {
+      setError("Name can't be blank.")
+      return;
+    }
+    if (interviewer === null) {
+      setError("Interviewer can't be unselected.")
+      return;
+    }
+    props.onSave(name, interviewer)
+    setError("");
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,12 +47,13 @@ export default function Form(props) {
             onChange={(event) => setName(event.target.value)} // can be short written to {e => setName(e.target.value)
           />
         </form>
+        <section className="validate_appointment">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel(name)}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={saveError}>Save</Button>
         </section>
       </section>
     </main>
